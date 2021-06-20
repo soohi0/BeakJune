821217539f5fed4ea0ffb76a0a7e6276e6fcd8cf
soohi0 <soohi0966@naver.com>
2021-06-20 23:26:51 +0900

    21.06.20

---
+++ 10960.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+int N, M;
+int main()
+{
+    cin >> N >> M;
+    for(int i = 0; i < M; i++)
+    {
+        int n1, n2;
+        cin >> n1 >> n2;
+
+        
+    }
+}
